b4b6449fedf73df82a847c4ba17aaeb98d8be370
kostyamrkostya <kobakostya20@vk.com>
2024-06-21 14:30:33 +0300

    add site

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const ECS_button = document.querySelector('.ECS_button')
+const YouTube = document.querySelector('.YouTube_button')
+
+ECS_button.onclick = () => {
+    alert("Bruh his button doesn't work yet")
+}
